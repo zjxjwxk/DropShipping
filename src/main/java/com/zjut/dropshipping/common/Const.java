444ffc202667b9d8acdb0f7f17442c3ad447dc79
zjxjwxk <zjxjwxk@gmail.com>
2019-01-30 00:18:02 +0800

    Const

---
+++ src/main/java/com/zjut/dropshipping/common/Const.java
@@ -0,0 +1,19 @@
+package com.zjut.dropshipping.common;
+
+/**
+ * 常量类
+ * @author zjxjwxk
+ */
+public class Const {
+
+    public static final String PHONE = "phone";
+    public static final String IDENTITY_NUMBER = "identityNumber";
+    public static final String EXTERNAL_SHOP = "externalShop";
+
+    public interface State {
+
+        String NORMAL = "正常";
+
+        String FROZEN = "冻结";
+    }
+}
