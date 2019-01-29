7323c024d0b5c62a6a9ff042fa8d29f7dc7ed3a0
zjxjwxk <zjxjwxk@gmail.com>
2019-01-30 00:30:09 +0800

    MD5Util

---
+++ src/main/java/com/zjut/dropshipping/utils/MD5Util.java
@@ -0,0 +1,62 @@
+package com.zjut.dropshipping.utils;
+
+import java.security.MessageDigest;
+
+
+/**
+ * @author zjxjwxk
+ */
+public class MD5Util {
+
+    private static String byteArrayToHexString(byte[] b) {
+        StringBuilder resultSb = new StringBuilder();
+        for (int i = 0; i < b.length; i++) {
+            resultSb.append(byteToHexString(b[i]));
+        }
+
+        return resultSb.toString();
+    }
+
+    private static String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0) {
+            n += 256;
+        }
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    /**
+     * 返回大写MD5
+     *
+     * @param origin 原始字符串
+     * @param charsetname 字符集
+     * @return 加密后的字符串
+     */
+    private static String MD5Encode(String origin, String charsetname) {
+        String resultString = null;
+        try {
+            resultString = new String(origin);
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            if (charsetname == null || "".equals(charsetname)) {
+                resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+            } else {
+                resultString = byteArrayToHexString(md.digest(resultString.getBytes(charsetname)));
+            }
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return resultString.toUpperCase();
+    }
+
+    public static String MD5EncodeUtf8(String origin) {
+        origin = origin + PropertiesUtil.getProperty("password.salt", "");
+        return MD5Encode(origin, "utf-8");
+    }
+
+
+    private static final String hexDigits[] = {"0", "1", "2", "3", "4", "5",
+            "6", "7", "8", "9", "a", "b", "c", "d", "e", "f"};
+
+}
