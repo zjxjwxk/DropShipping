3c16b1a10bba9d10402a7fde38311c609197175e
zjxjwxk <zjxjwxk@gmail.com>
2019-02-02 16:16:45 +0800

    GoodsDTO

---
+++ src/main/java/com/zjut/dropshipping/dto/GoodsDTO.java
@@ -0,0 +1,15 @@
+package com.zjut.dropshipping.dto;
+
+import lombok.Data;
+
+/**
+ * @author zjxjwxk
+ */
+@Data
+public class GoodsDTO {
+
+    private Integer goodsId;
+    private String name;
+    private Integer price;
+
+}
