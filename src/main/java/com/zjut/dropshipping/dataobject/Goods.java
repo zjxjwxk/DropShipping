84fd29c907c050deada36bfdfa825f4fdc27b758
zjxjwxk <zjxjwxk@gmail.com>
2019-02-02 16:16:37 +0800

    Goods entity

---
+++ src/main/java/com/zjut/dropshipping/dataobject/Goods.java
@@ -0,0 +1,26 @@
+package com.zjut.dropshipping.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author zjxjwxk
+ */
+@Entity
+@Data
+public class Goods {
+
+    @Id
+    private Integer goodsId;
+    private Integer producerId;
+    private Integer categoryId;
+    private String name;
+    private Integer price;
+    private Integer stock;
+    private String status;
+    private Date createTime;
+    private Date updateTime;
+}
