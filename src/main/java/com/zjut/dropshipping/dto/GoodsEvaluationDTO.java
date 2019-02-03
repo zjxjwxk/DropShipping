1d034833991d727bb8eff8740e5166543d6f6336
zjxjwxk <zjxjwxk@gmail.com>
2019-02-03 22:26:03 +0800

    add GoodsEvaluationDTO

---
+++ src/main/java/com/zjut/dropshipping/dto/GoodsEvaluationDTO.java
@@ -0,0 +1,15 @@
+package com.zjut.dropshipping.dto;
+
+import lombok.Data;
+
+/**
+ * @author zjxjwxk
+ */
+@Data
+public class GoodsEvaluationDTO {
+
+    private Integer agentId;
+    private String agentName;
+    private Integer level;
+    private String content;
+}
