3be1b12e12a20fcd8ed1b12524fc33effa056cd0
zjxjwxk <zjxjwxk@gmail.com>
2019-02-03 22:25:52 +0800

    add GoodsDetailDTO

---
+++ src/main/java/com/zjut/dropshipping/dto/GoodsDetailDTO.java
@@ -0,0 +1,23 @@
+package com.zjut.dropshipping.dto;
+
+import lombok.Data;
+
+/**
+ * @author zjxjwxk
+ */
+@Data
+public class GoodsDetailDTO {
+
+    private Integer goodsId;
+    private Integer producerId;
+    private Integer categoryId;
+    private String name;
+    private Double price;
+    private Integer stock;
+    private String status;
+
+//    private Double averageLevel;
+//    private List<GoodsEvaluationDTO> goodsEvaluationList;
+//
+//    private ProducerDTO producer;
+}
