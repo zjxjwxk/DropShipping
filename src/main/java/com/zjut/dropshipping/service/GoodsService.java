e76c54b1486f824ee6c5a89d5856a81b0043c214
zjxjwxk <zjxjwxk@gmail.com>
2019-02-03 22:24:28 +0800

    implement getDetail, getAvgLevel, getEvaluation and getProducer

--- src/main/java/com/zjut/dropshipping/service/GoodsService.java
+++ src/main/java/com/zjut/dropshipping/service/GoodsService.java
@@ -7,4 +7,12 @@ public interface GoodsService {
     ServerResponse getList(String keyword, Integer categoryId,
                                              Integer pageNum, Integer pageSize,
                                              String orderBy);
+
+    ServerResponse getDetail(Integer goodsId);
+
+    ServerResponse getAverageLevel(Integer goodsId);
+
+    ServerResponse getEvaluation(Integer goodsId);
+
+    ServerResponse getProducer(Integer goodsId);
 }
