a4878ea9e7156b167fb1a46662458e7dd6348d35
zjxjwxk <zjxjwxk@gmail.com>
2019-02-11 17:45:36 +0800

    add findByProducerIdInAndCategoryId

--- src/main/java/com/zjut/dropshipping/repository/GoodsRepository.java
+++ src/main/java/com/zjut/dropshipping/repository/GoodsRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 /**
  * @author zjxjwxk
  */
@@ -13,4 +15,6 @@ public interface GoodsRepository extends JpaRepository<Goods, Integer> {
     Page<Goods> findByCategoryId(Integer categoryId, Pageable pageable);
 
     Goods findByGoodsId(Integer goodsId);
+
+    Page<Goods> findByProducerIdInAndCategoryId(List<Integer> producerId, Integer categoryId, Pageable pageable);
 }
