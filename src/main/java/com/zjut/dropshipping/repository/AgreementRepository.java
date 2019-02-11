9bc142436248d8d7f9e12d5e0f4213e2f9b18e96
zjxjwxk <zjxjwxk@gmail.com>
2019-02-11 17:45:04 +0800

    add findProducerIdListByAgentIdAndState

--- src/main/java/com/zjut/dropshipping/repository/AgreementRepository.java
+++ src/main/java/com/zjut/dropshipping/repository/AgreementRepository.java
@@ -2,6 +2,7 @@ package com.zjut.dropshipping.repository;
 
 import com.zjut.dropshipping.dataobject.Agreement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
@@ -13,4 +14,7 @@ public interface AgreementRepository extends JpaRepository<Agreement, Integer> {
     Agreement findByProducerIdAndAgentId(Integer producerId, Integer agentId);
 
     List<Agreement> findByAgentIdAndState(Integer agentId, String state);
+
+    @Query("select producerId from Agreement where agentId = ?1 and state = ?2")
+    List<Integer> findProducerIdListByAgentIdAndState(Integer agentId, String state);
 }
