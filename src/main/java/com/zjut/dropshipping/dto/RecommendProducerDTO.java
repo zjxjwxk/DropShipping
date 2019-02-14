a8c59ca16bc1adee5c90ac8e8824e574ef7a4b0b
zjxjwxk <zjxjwxk@gmail.com>
2019-02-14 18:20:21 +0800

    add RecommendProducerDTO

---
+++ src/main/java/com/zjut/dropshipping/dto/RecommendProducerDTO.java
@@ -0,0 +1,13 @@
+package com.zjut.dropshipping.dto;
+
+import lombok.Data;
+
+/**
+ * @author ljx
+ */
+@Data
+public class RecommendProducerDTO {
+
+    private Integer id;
+    private String name;
+}
