a6f8621b2fb4091c182ec6513920f3ba9efe2889
zjxjwxk <zjxjwxk@gmail.com>
2019-02-16 00:06:07 +0800

    add @EntityListeners(AuditingEntityListener.class)

--- src/main/java/com/zjut/dropshipping/dataobject/GoodsEvaluation.java
+++ src/main/java/com/zjut/dropshipping/dataobject/GoodsEvaluation.java
@@ -2,8 +2,10 @@ package com.zjut.dropshipping.dataobject;
 
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
 import javax.persistence.Id;
 import javax.persistence.IdClass;
 import java.io.Serializable;
@@ -13,6 +15,7 @@ import java.util.Date;
  * @author zjxjwxk
  */
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Data
 @IdClass(GoodsEvaluation.class)
 public class GoodsEvaluation implements Serializable {
