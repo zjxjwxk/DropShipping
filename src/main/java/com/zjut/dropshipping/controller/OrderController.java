b21d6342457a49dbead0b7873c15a9eef25080ba
zjxjwxk <zjxjwxk@gmail.com>
2019-02-15 22:34:07 +0800

    add /agent_save_order

--- src/main/java/com/zjut/dropshipping/controller/OrderController.java
+++ src/main/java/com/zjut/dropshipping/controller/OrderController.java
@@ -1,10 +1,14 @@
 package com.zjut.dropshipping.controller;
 
+import com.zjut.dropshipping.common.Const;
+import com.zjut.dropshipping.common.ResponseCode;
 import com.zjut.dropshipping.common.ServerResponse;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import com.zjut.dropshipping.dataobject.Agent;
+import com.zjut.dropshipping.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * @author zjxjwxk
@@ -13,9 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 public class OrderController {
 
-    @PostMapping("/agent_add_order")
+    private final OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping("/agent_save_order")
     @ResponseBody
-    public ServerResponse agentAddOrder(Integer goodsId, Integer amount) {
-        return null;
+    public ServerResponse agentAddOrder(HttpSession session,
+                                        @RequestParam(required = false) Integer orderId,
+                                        Integer goodsId, Integer amount, String remark,
+                                        String buyerName, String buyerPhone, String address) {
+        Agent agent = (Agent) session.getAttribute(Const.CURRENT_AGENT);
+        if (agent == null) {
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
+        }
+        return orderService.agentSaveOrder(orderId, agent.getId(), goodsId, amount, remark, buyerName, buyerPhone, address);
     }
 }
