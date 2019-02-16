a1d525c7595f259818251eddcddd4f53f1cb6ea7
zjxjwxk <zjxjwxk@gmail.com>
2019-02-16 14:33:17 +0800

    add findByAgentId

--- src/main/java/com/zjut/dropshipping/repository/OrderRepository.java
+++ src/main/java/com/zjut/dropshipping/repository/OrderRepository.java
@@ -4,12 +4,16 @@ import com.zjut.dropshipping.dataobject.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 /**
  * @author zjxjwxk
  */
 public interface OrderRepository extends JpaRepository<Order, Integer> {
 
     Order findOneByOrderId(Integer orderId);
+
+    List<Order> findByAgentId(Integer agentId);
 
     @Query("select sum(amount) from Order where goodsId = ?1")
     Integer findAmountByGoodsId(Integer goodsId);
