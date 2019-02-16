6e6cd8d9c78a054d76ec6d1976fafd13c198b315
zjxjwxk <zjxjwxk@gmail.com>
2019-02-16 19:40:49 +0800

    add Logistic Entity

---
+++ src/main/java/com/zjut/dropshipping/dataobject/Logistic.java
@@ -0,0 +1,27 @@
+package com.zjut.dropshipping.dataobject;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * @author zjxjwxk
+ */
+@Entity
+@Data
+public class Logistic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer logisticId;
+    private Integer orderId;
+    private String name;
+    private String logisticNumber;
+    private String state;
+    private Date deliveryDate;
+    private Double price;
+}
