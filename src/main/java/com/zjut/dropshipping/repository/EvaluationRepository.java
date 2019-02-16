798014b46da01d06026164984f7129d102fcb176
zjxjwxk <zjxjwxk@gmail.com>
2019-02-16 11:16:03 +0800

    add EvaluationRepository

---
+++ src/main/java/com/zjut/dropshipping/repository/EvaluationRepository.java
@@ -0,0 +1,14 @@
+package com.zjut.dropshipping.repository;
+
+import com.zjut.dropshipping.dataobject.Evaluation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+/**
+ * @author ljx
+ */
+public interface EvaluationRepository extends JpaRepository<Evaluation, Integer> {
+
+    @Query("select avg(level) from Evaluation where direction = 1 and agentId = ?1")
+    Integer findLevelByAgentId(Integer agentId);
+}
