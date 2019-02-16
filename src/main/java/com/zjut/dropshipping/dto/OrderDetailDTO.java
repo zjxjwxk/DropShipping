0bb390f9895f562541c9d3fb3dad1a92b6c155be
zjxjwxk <zjxjwxk@gmail.com>
2019-02-16 19:41:36 +0800

    add some member variables

--- src/main/java/com/zjut/dropshipping/dto/OrderDetailDTO.java
+++ src/main/java/com/zjut/dropshipping/dto/OrderDetailDTO.java
@@ -2,6 +2,8 @@ package com.zjut.dropshipping.dto;
 
 import lombok.Data;
 
+import java.util.Date;
+
 /**
  * @author zjxjwxk
  */
@@ -9,12 +11,24 @@ import lombok.Data;
 public class OrderDetailDTO {
 
     private Integer orderId;
+    private String orderState;
+
     private Integer producerId;
     private String producerName;
     private String region;
+
+    private String goodsContent;
+    private Double goodsPrice;
 
     private String buyerName;
     private String buyerPhone;
     private String buyerAddress;
     private String buyerRemark;
+
+    private Integer logisticId;
+    private String logisticNumber;
+    private String logisticName;
+    private Double logisticPrice;
+    private String logisticState;
+    private Date deliveryDate;
 }
