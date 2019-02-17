a08ecefc59fed2d2df795ddd80c95927226cf342
zjxjwxk <zjxjwxk@gmail.com>
2019-02-17 19:26:13 +0800

    add @IdClass(OrderItemMultiKeys.class)

--- src/main/java/com/zjut/dropshipping/dataobject/OrderItem.java
+++ src/main/java/com/zjut/dropshipping/dataobject/OrderItem.java
@@ -3,15 +3,19 @@ package com.zjut.dropshipping.dataobject;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
 
 /**
  * @author zjxjwxk
  */
 @Entity
 @Data
+@IdClass(OrderItemMultiKeys.class)
 public class OrderItem {
 
-    private String orderId;
-    private String goodsId;
-    private String amount;
+    @Id
+    private Integer orderId;
+    private Integer goodsId;
+    private Integer amount;
 }
