d240a802a703de307de54fe65c68f7892c8c0d9f
zjxjwxk <zjxjwxk@gmail.com>
2019-02-17 19:25:06 +0800

    change parameters

--- src/main/java/com/zjut/dropshipping/controller/OrderController.java
+++ src/main/java/com/zjut/dropshipping/controller/OrderController.java
@@ -4,11 +4,13 @@ import com.zjut.dropshipping.common.Const;
 import com.zjut.dropshipping.common.ResponseCode;
 import com.zjut.dropshipping.common.ServerResponse;
 import com.zjut.dropshipping.dataobject.Agent;
+import com.zjut.dropshipping.dataobject.OrderItem;
 import com.zjut.dropshipping.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * @author zjxjwxk
@@ -28,13 +30,14 @@ public class OrderController {
     @ResponseBody
     public ServerResponse agentAddOrder(HttpSession session,
                                         @RequestParam(required = false) Integer orderId,
-                                        Integer goodsId, Integer amount, String remark,
-                                        String buyerName, String buyerPhone, String address) {
+                                        @RequestBody List<OrderItem> orderItemList,
+                                        String remark, String buyerName,
+                                        String buyerPhone, String address) {
         Agent agent = (Agent) session.getAttribute(Const.CURRENT_AGENT);
         if (agent == null) {
             return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(), "用户未登录");
         }
-        return orderService.agentSaveOrder(orderId, agent.getId(), goodsId, amount, remark, buyerName, buyerPhone, address);
+        return orderService.agentSaveOrder(orderId, agent.getId(), orderItemList, remark, buyerName, buyerPhone, address);
     }
 
     @GetMapping("/agent_get_order_list")
