19205bafba3402d9be30b35c43398790aeb91be4
zjxjwxk <zjxjwxk@gmail.com>
2019-02-17 19:26:46 +0800

    change variables

--- src/main/java/com/zjut/dropshipping/dto/OrderDetailDTO.java
+++ src/main/java/com/zjut/dropshipping/dto/OrderDetailDTO.java
@@ -1,8 +1,8 @@
 package com.zjut.dropshipping.dto;
 
+import com.zjut.dropshipping.dataobject.Buyer;
+import com.zjut.dropshipping.dataobject.Logistic;
 import lombok.Data;
-
-import java.util.Date;
 
 /**
  * @author zjxjwxk
@@ -10,25 +10,6 @@ import java.util.Date;
 @Data
 public class OrderDetailDTO {
 
-    private Integer orderId;
-    private String orderState;
-
-    private Integer producerId;
-    private String producerName;
-    private String region;
-
-    private String goodsContent;
-    private Double goodsPrice;
-
-    private String buyerName;
-    private String buyerPhone;
-    private String buyerAddress;
-    private String buyerRemark;
-
-    private Integer logisticId;
-    private String logisticNumber;
-    private String logisticName;
-    private Double logisticPrice;
-    private String logisticState;
-    private Date deliveryDate;
+    private Logistic logistic;
+    private Buyer buyer;
 }
