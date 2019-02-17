d4d497be063d8967b72b625d0e8ea9972b890ac7
zjxjwxk <zjxjwxk@gmail.com>
2019-02-17 19:26:25 +0800

    add OrderItemMultiKeys

---
+++ src/main/java/com/zjut/dropshipping/dataobject/OrderItemMultiKeys.java
@@ -0,0 +1,12 @@
+package com.zjut.dropshipping.dataobject;
+
+import java.io.Serializable;
+
+/**
+ * @author zjxjwxk
+ */
+class OrderItemMultiKeys implements Serializable {
+
+    private Integer orderId;
+    private Integer goodsId;
+}
