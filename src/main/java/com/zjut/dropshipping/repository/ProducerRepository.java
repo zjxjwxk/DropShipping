3a3d1c40189e565f200031d3cf9b3a3f24bed9d6
zjxjwxk <zjxjwxk@gmail.com>
2019-02-17 19:28:47 +0800

    add findIdAndNameById

--- src/main/java/com/zjut/dropshipping/repository/ProducerRepository.java
+++ src/main/java/com/zjut/dropshipping/repository/ProducerRepository.java
@@ -5,6 +5,7 @@ import com.zjut.dropshipping.dataobject.Producer;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /**
  * @author ljx
@@ -25,4 +26,7 @@ public interface ProducerRepository extends JpaRepository<Producer, Integer> {
 
     @Override
     Page<Producer> findAll(Pageable pageable);
+
+    @Query("select new Producer(producer.id, producer.name) from Producer producer where id = ?1")
+    Producer findIdAndNameById(Integer id);
 }
