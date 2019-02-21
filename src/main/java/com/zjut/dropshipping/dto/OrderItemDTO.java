792b470e76adbf27aa0a19d0f570496de312a67d
zjxjwxk <zjxjwxk@gmail.com>
2019-02-21 16:11:51 +0800

    add specificationList

--- src/main/java/com/zjut/dropshipping/dto/OrderItemDTO.java
+++ src/main/java/com/zjut/dropshipping/dto/OrderItemDTO.java
@@ -1,6 +1,9 @@
 package com.zjut.dropshipping.dto;
 
+import com.zjut.dropshipping.dataobject.Specification;
 import lombok.Data;
+
+import java.util.List;
 
 /**
  * @author zjxjwxk
@@ -10,6 +13,7 @@ public class OrderItemDTO {
 
     private Integer goodsId;
     private String name;
+    private List<Specification> specificationList;
     private Integer amount;
     private Double price;
 }
