84057a66be390f753f97bbeb687cca42fcf8c523
zjxjwxk <zjxjwxk@gmail.com>
2019-02-24 20:01:42 +0800

    add CartRepository

---
+++ src/main/java/com/zjut/dropshipping/repository/CartRepository.java
@@ -0,0 +1,14 @@
+package com.zjut.dropshipping.repository;
+
+import com.zjut.dropshipping.dataobject.ShoppingCart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * @author zjxjwxk
+ */
+public interface CartRepository extends JpaRepository<ShoppingCart, Integer> {
+
+    List<ShoppingCart> findByAgentId(Integer agentId);
+}
