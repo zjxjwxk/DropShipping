8eaa8a512c26b4e602d5af6d45f269478a4eff09
zjxjwxk <zjxjwxk@gmail.com>
2019-02-24 20:00:52 +0800

    add ShoppingCart

---
+++ src/main/java/com/zjut/dropshipping/dataobject/ShoppingCart.java
@@ -0,0 +1,25 @@
+package com.zjut.dropshipping.dataobject;
+
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.IdClass;
+import java.util.Date;
+
+/**
+ * @author zjxjwxk
+ */
+@Entity
+@Data
+@IdClass(ShoppingCartMultiKeys.class)
+public class ShoppingCart {
+
+    @Id
+    private Integer agentId;
+    private String goodsSpecIds;
+    private Integer amount;
+    @CreatedDate
+    private Date createTime;
+}
