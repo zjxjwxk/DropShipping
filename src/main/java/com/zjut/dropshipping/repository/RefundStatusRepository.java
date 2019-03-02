a5aa25ddee49efebc08b5bedf7fdd3f71abc336c
zjxjwxk <zjxjwxk@gmail.com>
2019-03-02 20:46:58 +0800

    add RefundStatusRepository

---
+++ src/main/java/com/zjut/dropshipping/repository/RefundStatusRepository.java
@@ -0,0 +1,12 @@
+package com.zjut.dropshipping.repository;
+
+import com.zjut.dropshipping.dataobject.RefundStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * @author zjxjwxk
+ */
+public interface RefundStatusRepository extends JpaRepository<RefundStatus, Integer> {
+
+
+}
